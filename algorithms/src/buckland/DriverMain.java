20bd2b3c7d950d7b8928069083e1b0384202c5d3
B B <barlimanbutterbur@gmail.com>
2012-07-16 22:58:06 -0700

    added new proj

---
+++ algorithms/src/buckland/DriverMain.java
@@ -0,0 +1,23 @@
+package buckland;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import shire.sort.ISort;
+import shire.sort.InsertionSort;
+
+public class DriverMain {
+	
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < 10; i++)
+			list.add(new Random().nextInt()%100);
+		
+		ISort<Integer> sort = new InsertionSort<Integer>();
+		list = sort.sort(list);
+		
+		System.out.println(list);
+	}
+
+}
---
+++ algorithms/src/shire/sort/InsertionSort.java
@@ -0,0 +1,37 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package shire.sort;
+
+import java.util.List;
+
+public class InsertionSort<T extends Comparable<? super T>> implements ISort<T> {
+
+	@Override
+	public List<T> sort(List<T> A) {
+		for (int i = 1; i < A.size(); i++) {
+			T key = A.get(i);
+			int j;
+			for (j = i - 1; j >= 0; j--) {
+				if (A.get(j).compareTo(key) > 0) {
+					A.set(j + 1, A.get(j));
+				} else {
+					break;
+				}
+			}
+			A.set(j + 1, key);
+		}
+		return A;
+	}
+}
